daeed1c3a76aa6740cbfdd796cf377184146adb0
Salomon May <sollymay@gmail.com>
2021-09-10 00:35:24 -0400

    added order details screen

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../providers/order_provider.dart' as ord;
+import 'package:intl/intl.dart';
+
+class OrderItem extends StatelessWidget {
+  final ord.OrderItem order;
+
+  OrderItem(this.order);
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(children: [
+        ListTile(
+          title: Text('\$${order.amount}'),
+          subtitle: Text(
+            DateFormat('dd MM yyyy hh:mm').format(order.dateTime),
+          ),
+          trailing: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.expand_more),
+          ),
+        ),
+      ]),
+    );
+  }
+}
