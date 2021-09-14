5fc5c5b2ee41cd2e349427bd2512a887dbf7f84c
Salomon May <sollymay@gmail.com>
2021-09-14 16:59:17 -0400

    added confirm removal dialog

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -46,6 +46,16 @@ class ProductItem extends StatelessWidget {
                 product.price,
                 product.title,
               );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('Added item to cart!'),
+                duration: Duration(seconds: 2),
+                action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    }),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),
